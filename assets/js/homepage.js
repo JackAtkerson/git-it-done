62cb4052cb6a56dd767f7290447c83cad7ea42a1
Jack Atkerson <jatkerson18@gmail.com>
2022-05-18 18:14:25 -0500

    Added error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,9 +9,13 @@ var getUserRepos = function(user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: Github User Not Found");
+        }
     });
 };
 
@@ -31,6 +35,12 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    //check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     //clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
